3950f3d7b68eef57642966faa5af36bc8859f4ac
tahfeezmizan <mizanahmed5316@gmail.com>
2025-12-15 06:43:10 +0600

    udpate redux provider

--- redux/ReduxProvider.tsx
+++ redux/ReduxProvider.tsx
@@ -1,15 +1,14 @@
 "use client";
 // import { PageLoading } from "@/components/shared/page-loading";
-import { persistor, store } from "@/redux/store";
+import { store } from "@/redux/store";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 
 const ReduxProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider store={store}>
       {/* <PersistGate loading={<PageLoading />} persistor={persistor}> */}
       {children}
-      {/* </PersistGate> */}
+      {/* </PersistGate> */}{" "}
     </Provider>
   );
 };
