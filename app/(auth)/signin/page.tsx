77d0bf0db68a8b1e3a19a4a3ed06bb5a23bc48bd
tahfeezmizan <mizanahmed5316@gmail.com>
2026-03-05 02:05:15 +0600

    update login page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,11 +1,14 @@
 import { AuthLayout } from "@/components/auth/auth-layout";
 import { SignInForm } from "@/components/auth/login-form";
+import { Suspense } from "react";
 
 export default function page() {
   return (
     <div>
       <AuthLayout>
-        <SignInForm />
+        <Suspense>
+          <SignInForm />
+        </Suspense>
       </AuthLayout>
     </div>
   );
